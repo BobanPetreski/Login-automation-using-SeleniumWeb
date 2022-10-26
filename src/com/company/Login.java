0360bdf9960be8a9c5863fe3f8181cc33802c171
Boban Petreski <bosh.petreski@gmail.com>
2022-10-26 12:14:29 +0200

    User login test

--- src/com/company/Login.java
+++ src/com/company/Login.java
@@ -3,7 +3,9 @@ package com.company;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+@Test
 public class Login {
 
     public static void main(String[] args) {
